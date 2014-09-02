c1a538eb1d5b7ba4267bedb8cf0f8ad10a20f71d
vinodprakash <psvinodprakash@gmail.com>
2014-09-02 18:03:37 +0530

    Adding images and layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   get 'home/index'
- get 'home/about'
-  get 'home/gallery'
-  get 'home/workshop' 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
