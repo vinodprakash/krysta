ae48b3f35bf2bcd44fb0fb21dd22b7205ebfd7e2
rinoldsimon <crisrinold@gmail.com>
2014-09-09 13:26:57 +0530

    validate phone number

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
 	validates :email, :email => {:strict_mode => true}
+	validates :mob_no, :mob_no => {:ten_digits => true, :seven_digits => true, :message => "Enter Valid Phone number"}
 end
